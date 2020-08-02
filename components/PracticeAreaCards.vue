85341b93d2615bb6ff0976c731bf98b137c5419f
FordanG <60175691+FordanG@users.noreply.github.com>
2020-08-02 23:48:24 +0800

    Added Icons for Practice Areas

---
+++ components/PracticeAreaCards.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <b-container fluid="lg">
+      <b-row align-h="center" class="flex-wrap">
+        <b-col
+          v-for="practiceArea in allPracticeAreas"
+          :key="practiceArea.practiceArea"
+          class="practiceArea d-flex flex-column text-center px-4 mx-0 my-2 col-12 col-md-4"
+        >
+          <icon class="icon-fluid" :src="practiceArea.icon" :alt="practiceArea.description" />
+          <h2>{{ practiceArea.description }}</h2>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      allPracticeAreas: [
+        {
+          icon: require('~/assets/images/practice-areas/corporate.svg'),
+          description: 'General Corporate',
+        },
+        {
+          icon: require('~/assets/images/practice-areas/structural.svg'),
+          description: 'Infrastructure',
+        },
+        {
+          icon: require('~/assets/images/practice-areas/government.svg'),
+          description: 'Government Contracts',
+        },
+        {
+          icon: require('~/assets/images/practice-areas/dispute.svg'),
+          description: 'Dispute Resolution',
+        },
+        {
+          icon: require('~/assets/images/practice-areas/tax.svg'),
+          description: 'Tax',
+        },
+        {
+          icon: require('~/assets/images/practice-areas/itech.svg'),
+          description: 'I-tech & Intellectual Property',
+        },
+        {
+          icon: require('~/assets/images/practice-areas/'),
+          description: 'Human Resources & Immigration',
+        },
+        {
+          icon: require('~/assets/images/practice-areas/'),
+          description: 'Fashion, Entertainment, & Media (FEM)',
+        },
+        {
+          icon: require('~/assets/images/practice-areas/'),
+          description: 'Innovation & Competition',
+        },
+      ],
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
