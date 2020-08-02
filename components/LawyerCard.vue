56cba7c27a55635e20211d841aec5ecae085293b
Daniel Fordan <danielfordan@me.com>
2020-08-03 05:07:07 +0800

    Fix lawyer cards

---
+++ components/LawyerCard.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="my-3">
+    <nuxt-link to="/">
+      <b-card
+        class="justify-content-between"
+        img-top
+        :img-src="image"
+        :title="name"
+      ></b-card>
+    </nuxt-link>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    image: { type: String, default: '' },
+    name: { type: String, default: '' },
+    email: { type: String, default: '' },
+  },
+  data() {
+    return {
+      isHovered: false,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+a,
+a:hover {
+  text-decoration: none;
+}
+
+.card {
+  transition: 0.5s ease;
+}
+.card:hover {
+  box-shadow: 0px 3px 6px 5px #0000002f;
+}
+
+.lawyer img {
+  object-fit: cover;
+  border-radius: 10px 10px 0 0;
+}
+
+.nameCard {
+  background: $primary;
+  border-radius: 0 0 10px 10px;
+  box-shadow: 0px 3px 6px 0px #0000000f;
+
+  h2 {
+    color: $accent;
+    text-transform: uppercase;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 1.25rem;
+  }
+
+  p {
+    color: var(--white);
+    font-style: normal;
+    font-weight: 400;
+    font-size: 0.875rem;
+  }
+}
+</style>
--- pages/lawyers.vue
+++ pages/lawyers.vue
@@ -1,12 +1,74 @@
 <template>
   <section id="lawyers">
-    <LawyerCards />
+    <b-container fluid="lg">
+      <b-row cols="1" cols-md="3" cols-lg="4" align-h="center" align-v="stetch">
+        <b-col v-for="lawyer in lawyers" :key="lawyer.name">
+          <lawyer-card
+            :image="lawyer.img"
+            :name="lawyer.name"
+            :email="lawyer.email"
+          >
+          </lawyer-card>
+        </b-col>
+      </b-row>
+    </b-container>
   </section>
 </template>
 
 <script>
 import Vue from 'vue'
-export default Vue.extend({})
+export default Vue.extend({
+  data() {
+    return {
+      lawyers: [
+        {
+          img: require('~/assets/images/lawyers/1-Melissa.jpg'),
+          name: 'Melissa Chavez',
+          email: 'melissa.chavez@gulapalaw.com',
+        },
+        {
+          img: require('~/assets/images/lawyers/2-Angel.jpg'),
+          name: 'Angel Machuca',
+          email: 'angelmachuca@gulapalaw.com',
+        },
+        {
+          img: require('~/assets/images/lawyers/3-Shwaid.jpg'),
+          name: 'Al-Shwaid Ismael',
+          email: 'alshwaid.ismael@gulapalaw.com',
+        },
+        {
+          img: require('~/assets/images/lawyers/4-Yas.jpg'),
+          name: 'Yas Sanchez',
+          email: 'yas.sanchez@gulapalaw.com',
+        },
+        {
+          img: require('~/assets/images/lawyers/5-Caloy.jpg'),
+          name: 'Caloy Cajucom',
+          email: 'caloy.cajucom@gulapalaw.com',
+        },
+        {
+          img: require('~/assets/images/lawyers/6-Aileen.jpg'),
+          name: 'Aileen Sanguir',
+          email: 'aileen.sanguir@gulapalaw.com',
+        },
+        {
+          img: require('~/assets/images/lawyers/7-Dake.jpg'),
+          name: 'Dake Mandocdoc',
+          email: 'dake.mandocdoc@gulapalaw.com',
+        },
+        {
+          img: require('~/assets/images/lawyers/8-Phil.jpg'),
+          name: 'Phil Recentes',
+          email: 'phil.@gulapalaw.com',
+        },
+      ],
+    }
+  },
+})
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.nuxt-link:hover {
+  text-decoration: none;
+}
+</style>
