49693ebd8c19e3254c72c262a647a118ddcb595d
Daniel Fordan <danielfordan@me.com>
2020-08-03 09:54:13 +0800

    Add lawyer slug using nuxt content

---
+++ pages/lawyer/_slug.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nuxt-content :document="info" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData({ $content, params }) {
+    const info = await $content(`lawyers/${params.slug}` || 'index').fetch()
+    return { info }
+  },
+})
+</script>
+
+<style scoped></style>
